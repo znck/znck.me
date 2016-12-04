8f63b2c63208e4b2dd5045a4c7319abd8b4ed0e9
Rahul Kadyan <hi@znck.me>
2016-12-04 19:15:59 +0530

    :tada: Move to sereno

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var elixir = require('laravel-elixir');
+var argv = require('yargs').argv;
+
+elixir.config.publicPath = 'content/assets';
+
+elixir(function (mix) {
+  var env = argv.e || argv.env || 'dev';
+
+  mix.sass(['app.scss'])
+      .browserify('app.js')
+      .exec('php sereno build --env=' + env, [
+        './blog/*',
+        './docs/*',
+        './content/*',
+        './blog/**/*',
+        './docs/**/*',
+        './content/**/*',
+        './resources/*',
+        './resources/**/*',
+        './config*.php'
+      ])
+      .browserSync({
+        server: {
+          baseDir: 'public'
+        },
+        proxy: null,
+        files: ['public/**/*']
+      });
+});
---
+++ resources/views/nav.blade.php
@@ -0,0 +1,15 @@
+<li class="nav-item">
+    <a class="nav-link @active('/')" href="@url('/')">Home</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link @active('blog')" href="@url('blog')">Blog</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link @active('projects')" href="@url('projects')">Projects</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="https://github.com/znck" target="_blank">Github</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link @active('about')" href="@url('about')">About</a>
+</li>
